c3349e43578ca74b27521e24db222de935f1566e
adelynntran <161761042+adelynntran@users.noreply.github.com>
2024-08-12 17:00:24 -0400

    Change the position of the size-choosing-container, have not fix the sketch grid first row problem, have not code the function of "Save color" and "Reverse all" button

--- Grid.js
+++ Grid.js
@@ -11,6 +11,7 @@ export default class Grid {
     createGrid() {
         //Calculate the size of each cell based on the grid size
         const cellSize = 960 / this.gridSize;
+        // const finalCellSize = cellSize - ((this.gridSize -1) / this.gridSize);
         this.gridElement.style.setProperty('--grid-size', this.gridSize);
         this.gridElement.style.setProperty('--cell-size', `${cellSize}px`);
 
@@ -18,15 +19,17 @@ export default class Grid {
         this.gridElement.innerHTML = '';
 
         //Create the grid
-        for (let i=0; i < this.gridSize * this.gridSize; i++) {
+        for (let i=1; i <= this.gridSize * this.gridSize; i++) {
             const box = document.createElement('div');
+            this.gridElement.appendChild(box);
+         
             box.classList.add('grid-box');
             box.addEventListener('click', () => {
                 if (this.currentColor) {
                     box.style.backgroundColor = this.currentColor;
                 }
             })
-            this.gridElement.appendChild(box);
+            
 
 
         }
--- script.js
+++ script.js
@@ -2,127 +2,126 @@
         
 import Grid from "./Grid.js";
 
-        document.addEventListener('DOMContentLoaded', () => {
-            const sketchContainer = document.getElementById('sketch-container');
-            const sizeInput = document.getElementById('size-choosing');
-            const sizeButton = document.getElementById('size-button');
-            const hexInput =document.getElementById('hex-code');
-            const submitButton = document.getElementById('submit-button');
-            const eraseButton = document.getElementById('erase-button');
-            const saveColorButton = document.getElementById('save-color-button');
-            const colorSelector = document.getElementsByClassName('color');
-            const gridBoxArray = [];
-            
+document.addEventListener('DOMContentLoaded', () => {
+    const sketchContainer = document.getElementById('sketch-container');
+    const sizeInput = document.getElementById('size-choosing');
+    const sizeButton = document.getElementById('size-button');
+    const hexInput =document.getElementById('hex-code');
+    const submitButton = document.getElementById('submit-button');
+    const eraseButton = document.getElementById('erase-button');
+    const saveColorButton = document.getElementById('save-color-button');
+    const colorSelector = document.getElementsByClassName('color');
+    const gridBoxArray = [];
+    
 
 
-            //initializ grid with default size
-            let currentGrid = new Grid(sketchContainer, 16);
-            let selectedColor = 'white';
+    //initializ grid with default size
+    let currentGrid = new Grid(sketchContainer, 16);
+    let selectedColor = 'white';
 
-            //size selection
-            sizeButton.addEventListener('click', () => {
-                const gridSize = parseInt(sizeInput.value, 10) || 16; //if input invalid, size changes to 16
-                currentGrid = new Grid(sketchContainer, gridSize);
-                currentGrid.setColor(selectedColor);
-                console.log(`Size ${gridSize} is choosen.`)
-            });
+    //size selection
+    sizeButton.addEventListener('click', () => {
+        const gridSize = parseInt(sizeInput.value, 10) || 16; //if input invalid, size changes to 16
+        currentGrid = new Grid(sketchContainer, gridSize);
+        currentGrid.setColor(selectedColor);
+        console.log(`Size ${gridSize} is choosen.`)
+    });
 
-            
+    
 
-            //change the color of the box:
-            for (let i = 0; i < colorSelector.length; i++) {
-                console.log(colorSelector[i].classList);
-                colorSelector[i].addEventListener('click', () => {
-                    const colorElement = event.currentTarget;
-                    const colorClass = colorElement.classList[1];
-                    const color = getColorName(colorClass);
-                    selectedColor = color;
-                    currentGrid.setColor(selectedColor);
-                    console.log(`Choosen color: ${selectedColor}`);
-                })
-            }
-        
-        function getColorName(color){
-            switch (color) {
-                case 'red':
-                    return 'red';
-                    break;
-                case 'orange':
-                    return 'orange';
-                    break;
-                case 'yellow':
-                    return 'yellow';
-                    break;
-                case 'green':
-                    return 'green';
-                    break;
-                case 'blue':
-                    return 'blue';
-                    break;
-                case 'indigo':
-                    return 'indigo';
-                    break;
-                case 'violet':
-                    return 'violet';
-                    break;
-                case 'black':
-                    return 'black';
-                    break;
-                case 'white':
-                    return 'white';
-                    break;
-                default:
-                    return 'white';
-            }
-        }
-            
-
-        
-
-        submitButton.addEventListener('click', () => {
-            let hexColor = hexInput.value;
-            if (!hexColor.includes('#')) {
-                hexColor = '#' + hexColor;
-            }
-            selectedColor = hexColor;
-            currentGrid.setColor(selectedColor)
-            console.log(`Color selected: ${selectedColor}`);
+    //change the color of the box:
+    for (let i = 0; i < colorSelector.length; i++) {
+        console.log(colorSelector[i].classList);
+        colorSelector[i].addEventListener('click', () => {
+            const colorElement = event.currentTarget;
+            const colorClass = colorElement.classList[1];
+            const color = getColorName(colorClass);
+            selectedColor = color;
+            currentGrid.setColor(selectedColor);
+            console.log(`Choosen color: ${selectedColor}`);
         })
-            
-            eraseButton.addEventListener('click', () => {
-                const boxes = sketchContainer.getElementsByClassName('grid-box');
-                for (let box of boxes) {
-                    box.style.backgroundColor = 'white';
-                }
-                console.log('Sketch has been erased.');
-            })
+    }
 
-        function addColorCircle() {
-            const defaultColor = document.getElementById('default-color');
-            let hexColor = hexInput.value;
-            if (!hexColor.includes('#')) {
-                hexColor = '#' + hexColor;
-            }
-            let newColor = document.createElement('div');
-            newColor.classList.add('color');
-            defaultColor.appendChild(newColor);
-            newColor.style.width = '50px';
-            newColor.style.height = '50px';
-            newColor.style.border = '1px solid black';
-            newColor.style.borderRadius = '100%';
-            newColor.style.backgroundColor = hexColor;
-        
-            console.log(`Color ${hexInput.value} is saved.`);
+function getColorName(color){
+    switch (color) {
+        case 'red':
+            return 'red';
+            break;
+        case 'orange':
+            return 'orange';
+            break;
+        case 'yellow':
+            return 'yellow';
+            break;
+        case 'green':
+            return 'green';
+            break;
+        case 'blue':
+            return 'blue';
+            break;
+        case 'indigo':
+            return 'indigo';
+            break;
+        case 'violet':
+            return 'violet';
+            break;
+        case 'black':
+            return 'black';
+            break;
+        case 'white':
+            return 'white';
+            break;
+        default:
+            return 'white';
+    }
+}
+    
 
+
+
+submitButton.addEventListener('click', () => {
+    let hexColor = hexInput.value;
+    if (!hexColor.includes('#')) {
+        hexColor = '#' + hexColor;
+    }
+    selectedColor = hexColor;
+    currentGrid.setColor(selectedColor)
+    console.log(`Color selected: ${selectedColor}`);
+})
+    
+    eraseButton.addEventListener('click', () => {
+        const boxes = sketchContainer.getElementsByClassName('grid-box');
+        for (let box of boxes) {
+            box.style.backgroundColor = 'white';
         }
+        console.log('Sketch has been erased.');
+    })
 
-        
-        saveColorButton.addEventListener('click', addColorCircle);
-            
-        }
+function addColorCircle() {
+    const defaultColor = document.getElementById('default-color');
+    let hexColor = hexInput.value;
+    if (!hexColor.includes('#')) {
+        hexColor = '#' + hexColor;
+    }
+    let newColor = document.createElement('div');
+    newColor.classList.add('color');
+    defaultColor.appendChild(newColor);
+    newColor.style.width = '50px';
+    newColor.style.height = '50px';
+    newColor.style.border = '1px solid black';
+    newColor.style.borderRadius = '100%';
+    newColor.style.backgroundColor = hexColor;
 
-    )
-        
-        
+    console.log(`Color ${hexInput.value} is saved.`);
 
-        
+}
+
+
+saveColorButton.addEventListener('click', addColorCircle);
+    
+}
+
+)
+
+
+
